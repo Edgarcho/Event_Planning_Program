207baf5a2640f79959dc21731f4262dbe0b724d5
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-09 11:10:08 -0800

    Third test failed

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -2,9 +2,11 @@ package models;
 
 public class Event {
     private int guest;
+    private int food;
 
     public Event(int guest, int food, int drink, int entertainment){
         this.guest = guest;
+        this.food = food;
     }
     public int getsGuest(){
         return this.guest;
