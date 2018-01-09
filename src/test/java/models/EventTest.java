bec4604a85409ea2488664f7067ab0433c3c94dc
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-09 09:21:10 -0800

    Create first test

---
+++ src/test/java/models/EventTest.java
@@ -0,0 +1,12 @@
+package models;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EventTest {
+
+    @Test
+    public void newEvent_instantiatesCorrectly() throws Exception {
+    }
+    Event testEvent = new Event(1);
+    assertEquals(true, testEvent instancesof Event);
+}
