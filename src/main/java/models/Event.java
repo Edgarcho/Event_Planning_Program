8a98581ee1dc38ed1e83a5a80ddd69c0210ad0bd
Edgar Hernandez <edgarchortiz@gmail.com>
2018-01-09 10:53:34 -0800

    Second test passed

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,11 +1,12 @@
 package models;
 
 public class Event {
+    private int guest;
 
     public Event(int guest, int food, int drink, int entertainment){
-
+        this.guest = guest;
     }
     public int getsGuest(){
-        return 0;
+        return this.guest;
     }
 }
